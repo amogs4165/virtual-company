e6f5d2563b2290345b961cf45914fb9ebd1b7772
amogs4165 <amoghgopi6@gmail.com>
2022-05-06 22:22:08 +0530

    chat sample

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SignIn from "./screens/admin/signIn";
 import Companies from "./screens/users/companies";
 import CompanyInfo from "./screens/users/companyDetails";
 import Home from "./screens/users/home";
+import Chat from "./screens/users/message";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/admin" element={<SignIn />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/companyinfo" element={<CompanyInfo/>}/>
+          <Route path="/messages" element={<Chat/>}/>
         </Routes>
 
     </BrowserRouter>
---
+++ src/screens/users/message.js
@@ -0,0 +1,12 @@
+import Header from "../../components/header/Header";
+import Message from "../../components/message/message";
+
+const Chat = () => {
+  return (
+    <>
+      <Header />
+      <Message />
+    </>
+  );
+};
+export default Chat;
